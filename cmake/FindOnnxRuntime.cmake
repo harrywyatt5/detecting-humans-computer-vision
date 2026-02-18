58d8a0822f755d1e4d99055c395747899157d896
Harry Wyatt <harrywyatt5@outlook.com>
2026-02-18 19:54:02 +0000

    Clear up the repo and implement cmake

---
+++ cmake/FindOnnxRuntime.cmake
@@ -0,0 +1,48 @@
+# Is a helper library for finding libraries on the system
+include(FindPackageHandleStandardArgs)
+
+file(GLOB ONNXRUNTIME_WC_INCLUDES 
+	"/opt/onnx*/include" 
+	"/usr/local/onnx*/include"
+	"$ENV{HOME}/onnx*/include"
+)
+file(GLOB ONNXRUNTIME_WC_LIBS
+	"/opt/onnx*/lib"
+	"/usr/local/onnx*/lib",
+	"$ENV{HOME}/onnx*/lib"
+)
+
+find_path(OnnxRuntime_INCLUDE_DIR
+	NAMES onnxruntime_cxx_api.h
+	HINTS
+		${ONNXRUNTIME_ROOT_DIR}/include
+		$ENV{ONNXRUNTIME_ROOT_DIR}/include
+	PATHS
+		${ONNXRUNTIME_WC_INCLUDES}
+		/usr/include
+		/usr/local/include
+)
+
+find_library(OnnxRuntime_LIBRARY
+	NAMES onnxruntime
+	HINTS
+		${ONNXRUNTIME_ROOT_DIR}/lib
+		$ENV{ONNXRUNTIME_ROOT_DIR}/lib
+	PATHS
+		${ONNXRUNTIME_WC_LIBS}
+		/usr/lib
+		/usr/local/lib
+)
+
+find_package_handle_standard_args(OnnxRuntime REQUIRED_VARS OnnxRuntime_LIBRARY OnnxRuntime_INCLUDE_DIR)
+
+if(OnnxRuntime_FOUND AND NOT TARGET OnnxRuntime::OnnxRuntime)
+    add_library(OnnxRuntime::OnnxRuntime UNKNOWN IMPORTED)
+    set_target_properties(OnnxRuntime::OnnxRuntime PROPERTIES
+        IMPORTED_LOCATION "${OnnxRuntime_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${OnnxRuntime_INCLUDE_DIR}"
+    )
+endif()
+
+mark_as_advanced(OnnxRuntime_INCLUDE_DIR OnnxRuntime_LIBRARY)
+
--- include/Sam3Model.h
+++ include/Sam3Model.h
@@ -3,11 +3,13 @@
 #include "TensorRTProviderBuilder.h"
 
 #include "OnnxSession.h"
+#include "TensorImage.h"
 #include "OnnxSessionPartial.h"
 #include <onnxruntime_c_api.h>
 #include <onnxruntime_cxx_api.h>
 #include <string>
 #include <memory>
+#include <vector>
 #include <optional>
 
 class Sam3Model {
@@ -21,4 +23,9 @@ private:
 	
 public:
 	Sam3Model(const OnnxSessionPartial& imageEncoder, const OnnxSessionPartial& textEncoder, const OnnxSessionPartial& decoder, TensorRTProviderBuilder& trtBuilder);
+	
+	std::vector<Ort::Value> runImageEncoder(const TensorImage& image);
+	// TODO: add C++ operands for other encoders/decoders
+	std::vector<Ort::Value> runTextEncoder();
+	std::vector<Ort::Value> runDecoder();
 };
