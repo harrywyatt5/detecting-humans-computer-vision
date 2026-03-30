c745fcfc47a3dea8e74888c9e15851be3114b6fe
root <Harry Wyatt>
2026-03-30 01:32:59 +0000

    Minor changes include anchoring numbers to top left bounding box

--- include/CudaDevice.h
+++ include/CudaDevice.h
@@ -9,11 +9,19 @@ private:
     cudaStream_t stream;
     cv::cuda::Stream openCVStream;
 public:
-    CudaDevice(int id);
+    enum class CudaStreamPriority {
+        LOWEST = 0,
+        MEDIUM = 1,
+        HIGHEST = 2,
+        LET_OS_DECIDE = 3
+    };
+    CudaDevice(int id, CudaStreamPriority priority);
 
     int getDeviceId() const;
     void switchCudaDevice() const;
     void waitForCompletion();
     cudaStream_t& getCudaStream();
     cv::cuda::Stream& getOpenCVCudaStream();
+
+    ~CudaDevice();
 };
