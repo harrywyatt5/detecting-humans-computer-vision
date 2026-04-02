9edf08f26591acfd9e2a74e312ec195df33fbbf5
Harry Wyatt <harrywyatt5@outlook.com>
2026-04-02 17:51:07 +0000

    Reconfigure calibration table creator to use a native TensorRT calibration table

--- scripts/CreateInt8CalibrationTable.py
+++ scripts/CreateInt8CalibrationTable.py
@@ -3,29 +3,55 @@ import argparse
 import os
 import numpy as np
 import cv2
-from onnxruntime.quantization import CalibrationDataReader, create_calibrator, write_calibration_table
+import pycuda.driver as cuda
+import pycuda.autoinit
+import tensorrt
 
-class Reader(CalibrationDataReader):
+class Calibrator(tensorrt.IInt8EntropyCalibrator2):
     def __init__(self, images_dir):
         super().__init__()
-        self.images = [os.path.join(images_dir, f) for f in os.listdir(images_dir)]
-        self.iter = iter(self.images)
-        self.input_name = "images"
-        self.input_shape = np.array([1, 3, 1008, 1008])
+        img_list = os.listdir(images_dir)
+        self.images = [os.path.join(images_dir, f) for f in img_list]
+        self.batch_size = 1
+        self.index = 0
+        self.size = len(img_list)
 
-    def get_next(self):
-        path = next(self.iter, None)
-        if path is None:
+        self.device_mem = cuda.mem_alloc(3 * 1008 * 1008 * 4) # x * y * 3 channels * 4 bytes for float32
+
+    def get_batch_size(self):
+        return self.batch_size
+
+    def get_batch(self, names):
+        if self.index >= self.size:
             return None
         
+        path = self.images[self.index]
+        print(f"Current processing {path}", flush=True)
+
         img = cv2.imread(path)
         img = cv2.resize(img, (1008, 1008), interpolation=cv2.INTER_LINEAR)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = (img.astype(np.float32) / 127.5) - 1.0
-
         img = np.transpose(img, (2, 0, 1))
-        input_data = np.expand_dims(img, axis=0).astype(np.float32)
-        return {self.input_name: input_data}
+        img = np.expand_dims(img, axis=0).astype(np.float32)
+        img = np.ascontiguousarray(img)
+
+        # Copy the image into the gpu
+        cuda.memcpy_htod(self.device_mem, img)
+        self.index += 1
+
+        return [int(self.device_mem)]
+
+    def read_calibration_cache(self):
+        # Throw if calibration cache already exists
+        if os.path.exists("calibration.cache"):
+            raise Exception("Calibration file already exists")
+        
+        return None
+    
+    def write_calibration_cache(self, cache):
+        with open("calibration.cache", "wb") as f:
+            f.write(cache)
 
 def create_arg_parser():
     parser = argparse.ArgumentParser()
@@ -33,6 +59,7 @@ def create_arg_parser():
     parser.add_argument("-e", "--encoder", required=True, help="The path to the ONNX Image encoder you wish to quantize")
 
     return parser
+
 
 def main():
     args = create_arg_parser().parse_args()
@@ -43,20 +70,22 @@ def main():
     if not os.path.exists(args.encoder):
         raise Exception("Model does not exist")
     
-    calibrator = create_calibrator(
-        model=args.encoder,
-        op_types_to_calibrate=[],
-        augmented_model_path="temp.onnx"
-    )
-    calibrator.set_execution_providers(["CPUExecutionProvider"])
+    logger = tensorrt.Logger(tensorrt.Logger.WARNING)
+    builder = tensorrt.Builder(logger)
+    network = builder.create_network(1 << int(tensorrt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
+    parser = tensorrt.OnnxParser(network, logger)
+    config = builder.create_builder_config()
 
-    data_reader = Reader(args.path)
-    calibrator.collect_data(data_reader=data_reader)
-    write_calibration_table(calibrator.compute_data(), dir=".", file_name="calibration.flatbuffers")
+    with open(args.encoder, "rb") as f:
+        parser.parse(f.read())
+    
+    config.set_flag(tensorrt.INT8)
+    config.int8_calibrator = Calibrator(args.path)
 
-    # Clean up temp file
-    if os.path.exists("temp.onnx"):
-        os.remove("temp.onnx")
+    print("Starting to build int8 cache")
+    builder.build_engine(network, config)
+    print("Finished!")
+
 
 if __name__ == "__main__":
     main()
